d40af1875d89ff3458b756e17742fb96b68118f6
Charles Martin <cmarti70@students.towson.edu>
2023-05-11 15:03:00 -0400

    modified Team component

--- components/team.js
+++ components/team.js
@@ -15,27 +15,27 @@ export default function Team() {
   return (
     <>
       <section id="team">
-        <div className="container-md">
-          <div className="flex-col text-center">
-            <h1 className="p-3">Meet Our Team</h1>
-            <div className="card-group justify-content-center">
-              {data.map((member) => {
-                return (
-                  <div key={member.name} className="w-full p-4">
-                    <div className="p-3 rounded shadow" id={styles.card}>
-                      <Image
-                        src={pic}
-                        className="rounded-circle bg-secondary w-50 h-50"
-                        alt="head shot"
-                      />
-                      <hr></hr>
-                      <p className="fw-light fs-3">{member.name}</p>
-                      <p className="text-secondary">{member.title}</p>
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
+        <div className="container">
+          <h1 className="p-4  text-center">Meet Our Team</h1>
+          <div className="card-group gap-5 justify-content-center">
+            {data.map((member) => {
+              return (
+                <div
+                  key={member.name}
+                  className="p-3 rounded shadow g-col-5"
+                  id={styles.card}
+                >
+                  <Image
+                    src={pic}
+                    className="rounded-circle mx-auto d-block bg-secondary w-50 h-50"
+                    alt="head shot"
+                  />
+                  <hr></hr>
+                  <p className="fw-light fs-3">{member.name}</p>
+                  <p className="text-secondary">{member.title}</p>
+                </div>
+              );
+            })}
           </div>
         </div>
       </section>
