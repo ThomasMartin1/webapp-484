19079c5b4c045fea4977a62442d0803231136cda
Devjon Gjoka <dgjoka1@students.towson.edu>
2023-05-13 00:52:26 -0400

    working on modal

--- pages/profile.js
+++ pages/profile.js
@@ -1,10 +1,74 @@
 import View from "../components/view";
-
+import { useSession } from "next-auth/react";
 export default function Profile() {
+
+  const {data:session}= useSession();
+
+
   return (
+   
     <>
       <View>
-        <div>profile page</div>
+        <div>
+          <div className="border justify-content-start "> 
+            <div className="m-auto">              
+              <img src= {session?.user?.image}></img>  
+            </div>
+            
+
+
+
+           
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>
+
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+            <div >           
+              <table className= "table">
+
+                <thead>
+                  <tr>
+                    <th scope="col">Monday</th>
+                    <th scope="col">Tuesday</th>
+                    <th scope="col">Wednesday</th>
+                    <th scope="col">Thursday</th>
+                    <th scope="col">Friday</th>
+                    <th scope="col">Saturday</th>
+                    <th scope="col">Sunday</th>
+
+                  </tr>
+                </thead>
+
+
+                
+            
+                
+              </table>
+            </div>
+          </div>
+        </div>
       </View>
     </>
   );
