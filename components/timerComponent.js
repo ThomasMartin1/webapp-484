ecf043405cd50eb30eeb6210371194e926fa8484
bseabol1 <99768961+bseabol1@users.noreply.github.com>
2023-05-14 19:21:58 -0400

    changed timerComponent to utilize states for functionality and some changes to calendar page and css

--- components/timerComponent.js
+++ components/timerComponent.js
@@ -18,131 +18,147 @@ class TimerComponent extends React.Component {
     this.start = this.start.bind(this);
     this.workInterval = this.workInterval.bind(this);
     this.restInterval = this.restInterval.bind(this);
-    this.set = this.set.bind(this);
+    this.set52 = this.set52.bind(this);
+    this.set25 = this.set25.bind(this);
     this.handleRTime = this.handleRTime.bind(this);
     this.handleWTime = this.handleWTime.bind(this);
   }
 
   handleWTime(e) {
-    this.setState({
-      timeLeft: 0,
-      workingTime: this.state.workingTime,
-      restingTime: this.state.restingTime,
-      wTime: e.target.value,
-      rTime: this.state.rTime,
-      submitState: this.state.submitState,
+    this.setState((prevState) => {
+      return {
+        timeLeft: 0,
+        workingTime: prevState.workingTime,
+        restingTime: prevState.restingTime,
+        wTime: e.target.value,
+        rTime: prevState.rTime,
+        submitState: prevState.submitState,
+      };
     });
   }
 
   handleRTime(e) {
-    this.setState({
-      timeLeft: 0,
-      workingTime: this.state.workingTime,
-      restingTime: this.state.restingTime,
-      wTime: this.state.wTime,
-      rTime: e.target.value,
-      submitState: this.state.submitState,
+    this.setState((prevState) => {
+      return {
+        timeLeft: 0,
+        workingTime: prevState.workingTime,
+        restingTime: prevState.restingTime,
+        wTime: prevState.wTime,
+        rTime: e.target.value,
+        submitState: prevState.submitState,
+      };
     });
   }
 
   start(e) {
     e.preventDefault();
-    // var work = document.forms["custom"]["wtime"].value;
-    // var rest = document.forms["custom"]["rtime"].value;
 
     if (this.state.submitState == "Start") {
-      //   document.getElementById("work-time").innerHTML = work;
-      //   document.getElementById("rest-time").innerHTML = rest;
-      //   document.getElementById("submit").value = "Stop";
-      this.setState({
-        timeLeft: 0,
-        workingTime: this.state.wTime,
-        restingTime: this.state.rTime,
-        wTime: this.state.wTime,
-        rTime: this.state.rTime,
-        submitState: "Stop",
+      this.setState((prevState) => {
+        return {
+          timeLeft: 0,
+          workingTime: prevState.wTime,
+          restingTime: prevState.rTime,
+          wTime: prevState.wTime,
+          rTime: prevState.rTime,
+          submitState: "Stop",
+        };
       });
       this.workInterval();
     } else if (this.state.submitState == "Stop") {
       //   document.getElementById("work-time").innerHTML = work;
       //   document.getElementById("rest-time").innerHTML = rest;
       //   document.getElementById("submit").value = "Stop";
-      this.setState({
-        timeLeft: 0,
-        workingTime: 0,
-        restingTime: 0,
-        wTime: this.state.wTime,
-        rTime: this.state.rTime,
-        submitState: "Start",
+      this.setState((prevState) => {
+        return {
+          timeLeft: 0,
+          workingTime: "",
+          restingTime: "",
+          wTime: prevState.wTime,
+          rTime: prevState.rTime,
+          submitState: "Start",
+        };
       });
     }
   }
 
   workInterval() {
     var currTime = 0;
-    var work = this.state.wTime;
-    var x = setInterval(function () {
+    var work = parseInt(this.state.wTime);
+    var x = setInterval(() => {
       var setTime = work * 60 * 1000;
       currTime++;
       var timeLeft = setTime - currTime * 1000;
       var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
       var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
       if (minutes < 10 && seconds < 10) {
-        this.setState({
-          timeLeft: 0,
-          workingTime: "0" + minutes + ":" + "0" + seconds,
-          restingTime: this.state.restingTime,
-          wTime: this.state.rTime,
-          rTime: this.state.rTime,
-          submitState: this.state.submitState,
+        this.setState((prevState) => {
+          return {
+            timeLeft: 0,
+            workingTime: "0" + minutes + ":" + "0" + seconds,
+            restingTime: prevState.restingTime,
+            wTime: prevState.rTime,
+            rTime: prevState.rTime,
+            submitState: prevState.submitState,
+          };
         });
       } else if (minutes < 10 && seconds >= 10) {
-        this.setState({
-          timeLeft: 0,
-          workingTime: "0" + minutes + ":" + seconds,
-          restingTime: this.state.restingTime,
-          wTime: this.state.wTime,
-          rTime: this.state.rTime,
-          submitState: this.state.submitState,
+        this.setState((prevState) => {
+          return {
+            timeLeft: 0,
+            workingTime: "0" + minutes + ":" + seconds,
+            restingTime: prevState.restingTime,
+            wTime: prevState.wTime,
+            rTime: prevState.rTime,
+            submitState: prevState.submitState,
+          };
         });
       } else if (minutes >= 10 && seconds < 10) {
-        this.setState({
-          timeLeft: 0,
-          workingTime: minutes + ":" + "0" + seconds,
-          restingTime: this.state.restingTime,
-          wTime: this.state.wTime,
-          rTime: this.state.rTime,
-          submitState: this.state.submitState,
+        this.setState((prevState) => {
+          return {
+            timeLeft: 0,
+            workingTime: minutes + ":" + "0" + seconds,
+            restingTime: prevState.restingTime,
+            wTime: prevState.wTime,
+            rTime: prevState.rTime,
+            submitState: prevState.submitState,
+          };
         });
-      } else
-        this.setState({
-          timeLeft: 0,
-          workingTime: minutes + ":" + seconds,
-          restingTime: this.state.restingTime,
-          wTime: this.state.wTime,
-          rTime: this.state.rTime,
-          submitState: this.state.submitState,
+      } else {
+        this.setState((prevState) => {
+          return {
+            timeLeft: 0,
+            workingTime: minutes + ":" + seconds,
+            restingTime: prevState.restingTime,
+            wTime: prevState.wTime,
+            rTime: prevState.rTime,
+            submitState: prevState.submitState,
+          };
         });
+      }
       if (timeLeft <= 0) {
         clearInterval(x);
-        this.setState({
-          timeLeft: 0,
-          workingTime: this.state.wTime,
-          restingTime: this.state.restingTime,
-          wTime: this.state.wTime,
-          rTime: this.state.rTime,
-          submitState: this.state.submitState,
+        this.setState((prevState) => {
+          return {
+            timeLeft: 0,
+            workingTime: prevState.wTime,
+            restingTime: prevState.restingTime,
+            wTime: prevState.wTime,
+            rTime: prevState.rTime,
+            submitState: prevState.submitState,
+          };
         });
         this.restInterval();
       } else if (this.state.submitState == "Start") {
-        document.getElementById("work-time").innerHTML = "";
-        this.setState({
-          timeLeft: 0,
-          workingTime: "",
-          restingTime: this.state.restingTime,
-          wTime: this.state.wTime,
-          rTime: this.state.rTime,
-          submitState: this.state.submitState,
+        this.setState((prevState) => {
+          return {
+            timeLeft: 0,
+            workingTime: "",
+            restingTime: prevState.restingTime,
+            wTime: prevState.wTime,
+            rTime: prevState.rTime,
+            submitState: prevState.submitState,
+          };
         });
         clearInterval(x);
       }
@@ -152,93 +168,110 @@ class TimerComponent extends React.Component {
   restInterval() {
     var currTime = 0;
     var rest = this.state.rTime;
-    var x = setInterval(function () {
+    var x = setInterval(() => {
       var setTime = rest * 60 * 1000;
       currTime++;
       var timeLeft = setTime - currTime * 1000;
       var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
       var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
       if (minutes < 10 && seconds < 10)
-        this.setState({
-          timeLeft: 0,
-          workingTime: this.state.workingTime,
-          restingTime: "0" + minutes + ":" + "0" + seconds,
-          wTime: this.state.wTime,
-          rTime: this.state.rTime,
-          submitState: this.state.submitState,
+        this.setState((prevState) => {
+          return {
+            timeLeft: 0,
+            workingTime: prevState.workingTime,
+            restingTime: "0" + minutes + ":" + "0" + seconds,
+            wTime: prevState.wTime,
+            rTime: prevState.rTime,
+            submitState: prevState.submitState,
+          };
         });
       else if (minutes < 10 && seconds >= 10)
-        this.setState({
-          timeLeft: 0,
-          workingTime: this.state.workingTime,
-          restingTime: "0" + minutes + ":" + seconds,
-          wTime: this.state.wTime,
-          rTime: this.state.rTime,
-          submitState: this.state.submitState,
+        this.setState((prevState) => {
+          return {
+            timeLeft: 0,
+            workingTime: prevState.workingTime,
+            restingTime: "0" + minutes + ":" + seconds,
+            wTime: prevState.wTime,
+            rTime: prevState.rTime,
+            submitState: prevState.submitState,
+          };
         });
       else if (minutes >= 10 && seconds < 10)
-        this.setState({
-          timeLeft: 0,
-          workingTime: this.state.workingTime,
-          restingTime: minutes + ":" + "0" + seconds,
-          wTime: this.state.wTime,
-          rTime: this.state.rTime,
-          submitState: this.state.submitState,
+        this.setState((prevState) => {
+          return {
+            timeLeft: 0,
+            workingTime: prevState.workingTime,
+            restingTime: minutes + ":" + "0" + seconds,
+            wTime: prevState.wTime,
+            rTime: prevState.rTime,
+            submitState: prevState.submitState,
+          };
         });
-      else
-        document.getElementById("rest-time").innerHTML =
-          minutes + ":" + seconds;
-      this.setState({
-        timeLeft: 0,
-        workingTime: this.state.workingTime,
-        restingTime: minutes + ":" + seconds,
-        wTime: this.state.wTime,
-        rTime: this.state.rtime,
-        submitState: this.state.submitState,
-      });
+      else {
+        this.setState((prevState) => {
+          return {
+            timeLeft: 0,
+            workingTime: prevState.workingTime,
+            restingTime: minutes + ":" + seconds,
+            wTime: prevState.wTime,
+            rTime: prevState.rtime,
+            submitState: prevState.submitState,
+          };
+        });
+      }
       if (timeLeft <= 0) {
         clearInterval(x);
-        this.setState({
-          timeLeft: 0,
-          workingTime: this.state.workingTime,
-          restingTime: this.state.rTime,
-          wTime: this.state.wTime,
-          rTime: this.state.rTime,
-          submitState: this.state.submitState,
+        this.setState((prevState) => {
+          return {
+            timeLeft: 0,
+            workingTime: prevState.workingTime,
+            restingTime: prevState.rTime,
+            wTime: prevState.wTime,
+            rTime: prevState.rTime,
+            submitState: prevState.submitState,
+          };
         });
-        workInterval();
+        this.workInterval();
       } else if (this.state.submitState == "Start") {
-        this.setState({
-          timeLeft: 0,
-          workingTime: this.state.workingTime,
-          restingTime: "",
-          wTime: this.state.wTime,
-          rTime: this.state.rTime,
-          submitState: this.state.submitState,
+        this.setState((prevState) => {
+          return {
+            timeLeft: 0,
+            workingTime: prevState.workingTime,
+            restingTime: "",
+            wTime: prevState.wTime,
+            rTime: prevState.rTime,
+            submitState: prevState.submitState,
+          };
         });
         clearInterval(x);
       }
     }, 1000);
   }
 
-  set(x) {
-    if (x == 52) {
-      this.setState({
+  set52() {
+    this.setState((prevState) => {
+      return {
         timeLeft: 0,
-        workingTime: 0,
-        restingTime: 0,
+        workingTime: prevState.workingTime,
+        restingTime: prevState.restingTime,
         wTime: "52",
         rTime: "17",
-      });
-    } else if (x == 25) {
-      this.setState({
+        submitState: prevState.submitState,
+      };
+    });
+  }
+
+  set25() {
+    this.setState((prevState) => {
+      return {
         timeLeft: 0,
-        workingTime: 52,
-        restingTime: 17,
+        workingTime: prevState.workingTime,
+        restingTime: prevState.restingTime,
         wTime: "25",
         rTime: "5",
-      });
-    }
+        submitState: prevState.submitState,
+      };
+    });
   }
 
   render() {
@@ -247,11 +280,11 @@ class TimerComponent extends React.Component {
         <div className={styles.sidebar}>
           <span className={styles.timer}>Timer Presets:</span>
           <br></br>
-          <button type="button" onClick={this.set(52)}>
+          <button type="button" onClick={this.set52}>
             <span id="52">52/17 Rule</span>
           </button>
           <br></br>
-          <button type="button" onClick={this.set(25)}>
+          <button type="button" onClick={this.set25}>
             <span id="Pomo">Pomodoro</span>
           </button>
           <br></br>
--- pages/calendar.js
+++ pages/calendar.js
@@ -29,58 +29,68 @@ export default function Calendar() {
         <article className={styles.article}>
           <table className={styles.calendarTable}>
             <tr className={styles.tr}>
-              <th className={styles.th}>30</th>
-              <th className={styles.th}>1</th>
-              <th className={styles.th}>2</th>
-              <th className={styles.th}>3</th>
-              <th className={styles.th}>4</th>
-              <th className={styles.th}>5</th>
-              <th className={styles.th}>6</th>
+              <td className={styles.td}>
+                <div>
+                  <h5>30</h5>
+                  <span className={styles.dueDate}>COSC 484, Assignme...</span>
+                  <span className={styles.dueDate}>COSC 484, Assignment 2</span>
+                  <span className={styles.dueDate}>COSC 484, Assignment 2</span>
+                  <br />
+                  <span className={styles.dueDate}>COSC 484, Assignment 2</span>
+                  <br />
+                </div>
+              </td>
+              <td className={styles.td}>1</td>
+              <td className={styles.td}>2</td>
+              <td className={styles.td}>3</td>
+              <td className={styles.td}>4</td>
+              <td className={styles.td}>5</td>
+              <td className={styles.td}>6</td>
             </tr>
             <tr className={styles.tr}>
-              <th className={styles.th}>7</th>
-              <th className={styles.th}>8</th>
-              <th className={styles.th}>9</th>
-              <th className={styles.th}>10</th>
-              <th className={styles.th}>11</th>
-              <th className={styles.th}>12</th>
-              <th className={styles.th}>13</th>
+              <td className={styles.td}>7</td>
+              <td className={styles.td}>8</td>
+              <td className={styles.td}>9</td>
+              <td className={styles.td}>10</td>
+              <td className={styles.td}>11</td>
+              <td className={styles.td}>12</td>
+              <td className={styles.td}>13</td>
             </tr>
             <tr className={styles.tr}>
-              <th className={styles.th}>14</th>
-              <th className={styles.th}>15</th>
-              <th className={styles.th}>16</th>
-              <th className={styles.th}>17</th>
-              <th className={styles.th}>18</th>
-              <th className={styles.th}>19</th>
-              <th className={styles.th}>20</th>
+              <td className={styles.td}>14</td>
+              <td className={styles.td}>15</td>
+              <td className={styles.td}>16</td>
+              <td className={styles.td}>17</td>
+              <td className={styles.td}>18</td>
+              <td className={styles.td}>19</td>
+              <td className={styles.td}>20</td>
             </tr>
             <tr className={styles.tr}>
-              <th className={styles.th}>21</th>
-              <th className={styles.th}>22</th>
-              <th className={styles.th}>23</th>
-              <th className={styles.th}>24</th>
-              <th className={styles.th}>25</th>
-              <th className={styles.th}>26</th>
-              <th className={styles.th}>27</th>
+              <td className={styles.td}>21</td>
+              <td className={styles.td}>22</td>
+              <td className={styles.td}>23</td>
+              <td className={styles.td}>24</td>
+              <td className={styles.td}>25</td>
+              <td className={styles.td}>26</td>
+              <td className={styles.td}>27</td>
             </tr>
             <tr className={styles.tr}>
-              <th className={styles.th}>28</th>
-              <th className={styles.th}>29</th>
-              <th className={styles.th}>30</th>
-              <th className={styles.th}>31</th>
-              <th className={styles.th}>1</th>
-              <th className={styles.th}>2</th>
-              <th className={styles.th}>3</th>
+              <td className={styles.td}>28</td>
+              <td className={styles.td}>29</td>
+              <td className={styles.td}>30</td>
+              <td className={styles.td}>31</td>
+              <td className={styles.td}>1</td>
+              <td className={styles.td}>2</td>
+              <td className={styles.td}>3</td>
             </tr>
             <tr className={styles.tr}>
-              <th className={styles.th}>4</th>
-              <th className={styles.th}>5</th>
-              <th className={styles.th}>6</th>
-              <th className={styles.th}>7</th>
-              <th className={styles.th}>8</th>
-              <th className={styles.th}>9</th>
-              <th className={styles.th}>10</th>
+              <td className={styles.td}>4</td>
+              <td className={styles.td}>5</td>
+              <td className={styles.td}>6</td>
+              <td className={styles.td}>7</td>
+              <td className={styles.td}>8</td>
+              <td className={styles.td}>9</td>
+              <td className={styles.td}>10</td>
             </tr>
           </table>
         </article>
