3fc9341d732c055a140ef58a6d9e6db401fb04ca
bseabol1 <99768961+bseabol1@users.noreply.github.com>
2023-05-15 16:46:31 -0400

    calendar css edits and changed table data a bit

--- pages/calendar.js
+++ pages/calendar.js
@@ -1,5 +1,12 @@
 import AccountNav from "../components/accountnav";
 import styles from "../styles/Calendar.module.css";
+import ReactDom from "react-dom";
+import React from "react";
+
+function test() {
+  const test = React.createElement("span", null, "test Assignment");
+  ReactDom.render(test, document.getElementById("2"));
+}
 
 export default function Calendar() {
   return (
@@ -7,8 +14,8 @@ export default function Calendar() {
       <AccountNav />
       <section className={styles.section}>
         <aside className={styles.aside}>
+          <button onClick={test}>create event</button>
           <ul className={styles.asideUl}>
-            <li>create event</li>
             <li>
               <label for="class-1">Class 1:</label>
               <input type="checkbox" name="class-1" />
@@ -30,67 +37,229 @@ export default function Calendar() {
           <table className={styles.calendarTable}>
             <tr className={styles.tr}>
               <td className={styles.td}>
-                <div>
+                <div id="1">
                   <h5>30</h5>
-                  <span className={styles.dueDate}>COSC 484, Assignme...</span>
                   <span className={styles.dueDate}>COSC 484, Assignment 2</span>
                   <span className={styles.dueDate}>COSC 484, Assignment 2</span>
-                  <br />
                   <span className={styles.dueDate}>COSC 484, Assignment 2</span>
-                  <br />
+                  <span className={styles.dueDate}>COSC 484, Assignment 2</span>
                 </div>
               </td>
-              <td className={styles.td}>1</td>
-              <td className={styles.td}>2</td>
-              <td className={styles.td}>3</td>
-              <td className={styles.td}>4</td>
-              <td className={styles.td}>5</td>
-              <td className={styles.td}>6</td>
+              <td className={styles.td}>
+                <div id="2">
+                  <h5>30</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="3">
+                  <h5>1</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="4">
+                  <h5>2</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="5">
+                  <h5>3</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="6">
+                  <h5>4</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="7">
+                  <h5>5</h5>
+                </div>
+              </td>
             </tr>
             <tr className={styles.tr}>
-              <td className={styles.td}>7</td>
-              <td className={styles.td}>8</td>
-              <td className={styles.td}>9</td>
-              <td className={styles.td}>10</td>
-              <td className={styles.td}>11</td>
-              <td className={styles.td}>12</td>
-              <td className={styles.td}>13</td>
+              <td className={styles.td}>
+                <div id="8">
+                  <h5>6</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="9">
+                  <h5>7</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="10">
+                  <h5>8</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="11">
+                  <h5>9</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="12">
+                  <h5>10</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="13">
+                  <h5>11</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="14">
+                  <h5>12</h5>
+                </div>
+              </td>
             </tr>
             <tr className={styles.tr}>
-              <td className={styles.td}>14</td>
-              <td className={styles.td}>15</td>
-              <td className={styles.td}>16</td>
-              <td className={styles.td}>17</td>
-              <td className={styles.td}>18</td>
-              <td className={styles.td}>19</td>
-              <td className={styles.td}>20</td>
+              <td className={styles.td}>
+                <div id="15">
+                  <h5>30</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="16">
+                  <h5>30</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="17">
+                  <h5>30</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="18">
+                  <h5>30</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="19">
+                  <h5>30</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="20">
+                  <h5>30</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="21">
+                  <h5>30</h5>
+                </div>
+              </td>
             </tr>
             <tr className={styles.tr}>
-              <td className={styles.td}>21</td>
-              <td className={styles.td}>22</td>
-              <td className={styles.td}>23</td>
-              <td className={styles.td}>24</td>
-              <td className={styles.td}>25</td>
-              <td className={styles.td}>26</td>
-              <td className={styles.td}>27</td>
+              <td className={styles.td}>
+                <div id="22">
+                  <h5>30</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="23">
+                  <h5>30</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="24">
+                  <h5>30</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="25">
+                  <h5>30</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="26">
+                  <h5>30</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="27">
+                  <h5>30</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="28">
+                  <h5>30</h5>
+                </div>
+              </td>
             </tr>
             <tr className={styles.tr}>
-              <td className={styles.td}>28</td>
-              <td className={styles.td}>29</td>
-              <td className={styles.td}>30</td>
-              <td className={styles.td}>31</td>
-              <td className={styles.td}>1</td>
-              <td className={styles.td}>2</td>
-              <td className={styles.td}>3</td>
+              <td className={styles.td}>
+                <div id="29">
+                  <h5>30</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="30">
+                  <h5>30</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="31">
+                  <h5>30</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="32">
+                  <h5>30</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="33">
+                  <h5>30</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="34">
+                  <h5>30</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="35">
+                  <h5>30</h5>
+                </div>
+              </td>
             </tr>
             <tr className={styles.tr}>
-              <td className={styles.td}>4</td>
-              <td className={styles.td}>5</td>
-              <td className={styles.td}>6</td>
-              <td className={styles.td}>7</td>
-              <td className={styles.td}>8</td>
-              <td className={styles.td}>9</td>
-              <td className={styles.td}>10</td>
+              <td className={styles.td}>
+                <div id="36">
+                  <h5>30</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="37">
+                  <h5>30</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="38">
+                  <h5>30</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="39">
+                  <h5>30</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="40">
+                  <h5>30</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="41">
+                  <h5>30</h5>
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div id="42">
+                  <h5>30</h5>
+                </div>
+              </td>
             </tr>
           </table>
         </article>
