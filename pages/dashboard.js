a0354025a6cee2f0a5ef9b412a2d774413ddec09
bseabol1 <99768961+bseabol1@users.noreply.github.com>
2023-05-16 17:20:07 -0400

    Clean up the calendar page a little bit and moved it the items displaying function to dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,14 +1,39 @@
 import View from "../components/view";
 import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Dashboard() {
   const { data: session } = useSession();
+  const [items, setItems] = useState([]);
+  const email = session?.user.email;
+
+  useEffect(() => {
+    axios.get("/api/items").then((response) => {
+      setItems(response.data);
+    });
+  }, []);
 
   return (
     <>
       <View>
-        <div>{session?.user.name} dashboard</div>
+        <h1>{session?.user.name}'s Dashboard</h1>
+        {showItems()}
       </View>
     </>
   );
+
+  function showItems() {
+    return (
+      <>
+        {items.map((item, idx) => (
+          <div className="rounded shadow border m-3 p-2 w-25" key={idx}>
+            <div>{item.type}</div>
+            <div>{item.title}</div>
+            <div>{item.date}</div>
+          </div>
+        ))}
+      </>
+    );
+  }
 }
